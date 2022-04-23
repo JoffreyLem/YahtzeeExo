ac893c098ed33e19672b61225363dd62b6bebe9b
Joffrey LEMERY <lemery.joffrey@outlook.fr>
2022-04-23 23:40:06 +0200

    Ajout des rounds

---
+++ YahtzeeExo/Round.cs
@@ -0,0 +1,19 @@
+﻿namespace TestProjectYahtzee;
+
+public class Round
+{
+    private readonly IConsole console;
+
+    public Round(IConsole console)
+    {
+        this.console = console;
+    }
+
+    public void PlayAllRound()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            console.Print($"Round {i}");
+        }
+    }
+}
--- YahtzeeExo/Yahtzee.cs
+++ YahtzeeExo/Yahtzee.cs
@@ -4,17 +4,16 @@ public class Yahtzee
 {
 
     private readonly IConsole _console;
-
+    public Round Round { get; set; }
     public Yahtzee(IConsole console)
     {
         _console = console;
+        Round = new Round(_console);
     }
 
     public void Jouer()
     {
-        _console.Print("Round 1");
-        _console.Print("Round 2");
-        _console.Print("Round 3");
+       Round.PlayAllRound();
     }
 
 
