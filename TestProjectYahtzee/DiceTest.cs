ea1f77b7e9f6858b1dde8f29560d944de3df4df3
Joffrey LEMERY <lemery.joffrey@outlook.fr>
2022-04-23 23:53:44 +0200

    Ajout lancée  de dès

---
+++ TestProjectYahtzee/DiceTest.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using FluentAssertions.Execution;
+using NSubstitute;
+
+namespace TestProjectYahtzee;
+
+[TestFixture]
+public class DiceTest
+{
+    [Test]
+    public void LancerDice()
+    {
+        var dice = Substitute.For<Dice>();
+   
+        var desValeur = dice.Lancer();
+        desValeur.Should().BeGreaterOrEqualTo(1);
+        desValeur.Should().BeLessOrEqualTo(6);
+
+
+    }
+}
---
+++ YahtzeeExo/Dice.cs
@@ -0,0 +1,9 @@
+﻿namespace TestProjectYahtzee;
+
+public class Dice
+{
+    public int Lancer()
+    {
+        return new Random().Next(1,6); 
+    }
+}
